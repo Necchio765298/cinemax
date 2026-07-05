6b6a9843347e5aef58d2cb5ddf3cc1a820091890
GaiaGalimberti <gaiagali2006@gmail.com>
2026-07-05 12:04:04 +0200

    javadoc ma manca l'autore della classe
    Added Javadoc comments to class Proiezione and its methods.

--- Proiezione.java
+++ Proiezione.java
@@ -2,37 +2,78 @@
 import java.time.LocalDateTime;
 import java.io.Console;
 
+/**
+ * Rappresenta una proiezione cinematografica programmata dal cinema.
+ * La classe memorizza il film proiettato, la data e l'ora della
+ * proiezione e il prezzo del biglietto.
+ * @author 
+ */
 public class Proiezione {
-    private Film film;
-    private LocalDateTime dataOra;
-    private double prezzoBiglietto;
+   /** Film associato alla proiezione. */
+	private Film film;
+    /** Data e ora della proiezione. */
+	private LocalDateTime dataOra;
+    /** Prezzo del biglietto della proiezione. */
+	private double prezzoBiglietto;
 
+	/**
+ * Costruisce una nuova proiezione.
+ * @param film film associato alla proiezione
+ * @param dataOra data e ora della proiezione
+ * @param prezzoBiglietto prezzo del biglietto
+ */
     public Proiezione(Film film, LocalDateTime dataOra , double prezzoBiglietto){
         this.film = film;
         this.dataOra = dataOra ;
         this.prezzoBiglietto=prezzoBiglietto;
     }
 
+	/**
+ * Restituisce il film associato alla proiezione.
+ * @return il film della proiezione
+ */
     public Film getFilm(){
         return film;
     }
 
+	/**
+ * Restituisce la data e l'ora della proiezione.
+ * @return la data e l'ora della proiezione
+ */
     public LocalDateTime getDataOra(){
         return dataOra;
     }
 
+	/**
+ * Restituisce il prezzo del biglietto della proiezione.
+ * @return il prezzo del biglietto
+ */
     public double getPrezzoBiglietto(){
         return prezzoBiglietto;
     }
 
+	/**
+ * Modifica la data e l'ora della proiezione.
+ * @param nuova data e ora della proiezione
+ */
     public void setDataOra(LocalDateTime dataOra){
         this.dataOra=dataOra;
     }
 
+	/**
+ * Modifica il prezzo del biglietto della proiezione.
+ * @param nuovo prezzo del biglietto
+ */
     public void setPrezzoBiglietto(double prezzoBiglietto){
         this.prezzoBiglietto = prezzoBiglietto;
     }
 
+	/**
+ * Restituisce una rappresentazione testuale della proiezione,
+ * utilizzata per la visualizzazione delle informazioni e per
+ * la memorizzazione dei dati nel file csv.
+ * @return una stringa contenente i dati della proiezione
+ */
     public String toString(){
         return dataOra+ ","+ film.toString()+ "," +prezzoBiglietto;
     }
