c4d38b9640beb86dd32adac114f02b444c822954
GaiaGalimberti <gaiagali2006@gmail.com>
2026-07-05 11:41:29 +0200

    javadoc ma manca autore
    Added JavaDoc comments for better documentation of the Login class and its methods.

--- Login.java
+++ Login.java
@@ -6,11 +6,26 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 
+/**
+ * Gestisce l'autenticazione degli utenti dell'applicazione.
+ * La classe verifica le credenziali di accesso e determina
+ * il ruolo associato all'utente.
+ *
+ * @author 
+ */
 public class Login{
 	//campi
+	/** Nome utente utilizzato per l'accesso. */
 	String username;
+	/** Password associata all'utente. */
 	String password;
 
+	/**
+ * Costruisce un nuovo oggetto Login.
+ *
+ * @param username nome utente
+ * @param password password dell'utente
+ */
 	//costruttore
 	public Login(String username, String password){
 		this.username = username;
@@ -18,10 +33,24 @@ public class Login{
 	}
 
 	//metodi
+
+	/**
+ * Restituisce una rappresentazione testuale dell'oggetto Login.
+ *
+ * @return una stringa contenente le informazioni dell'oggetto
+ */
 	public String toString(){
 		return username + "," + password;
 	}
-	
+
+	/**
+ * Verifica la validità delle credenziali inserite confrontandole
+ * con quelle memorizzate nel file degli utenti.
+ *
+ * @param login oggetto contenente le credenziali da verificare
+ * @return true se l'autenticazione va a buon fine, false altrimenti
+ * @throws IOException se si verifica un errore durante la lettura del file
+ */
 	public static boolean login(Login login) throws IOException{
 		FileReader frd = new FileReader("../data/utenti.csv");
 		BufferedReader brd = new BufferedReader(frd);
@@ -40,7 +69,15 @@ public class Login{
 		}
 		return accesso;
 	}
-	
+
+	/**
+ * Restituisce il ruolo associato all'utente autenticato.
+ *
+ * @param login oggetto contenente le credenziali dell'utente
+ * @param accesso esito dell'autenticazione
+ * @return il ruolo dell'utente
+ * @throws IOException se si verifica un errore durante la lettura del file
+ */
 	public static String ruolo(Login login, boolean accesso) throws IOException{
 		FileReader frd = new FileReader("../data/utenti.csv");
 		BufferedReader brd = new BufferedReader(frd);
