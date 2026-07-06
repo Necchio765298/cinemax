600c0c63dfd152f34815ec432cd644d8e0f9db20
GaiaGalimberti <gaiagali2006@gmail.com>
2026-07-06 21:00:50 +0200

    javadoc ma manca l'autore della classe

--- Film.java
+++ Film.java
@@ -1,6 +1,5 @@
 //package bin;
-/**
- * Rappresenta un film presente nel catalogo del cinema.
+/** Rappresenta un film presente nel catalogo del cinema.
  * La classe memorizza le informazioni principali di ciascun film.
  *  @author
  */
@@ -19,8 +18,7 @@ public class Film {
     /** Età minima del film. */
     private int etaMinima;
 
-    /**
- * Costruisce un nuovo film.
+    /** Costruisce un nuovo film.
  * @param <titolo> titolo del film
  * @param <genere> genere del film
  * @param <regista> regista del film
@@ -38,56 +36,49 @@ public class Film {
         this.etaMinima=etaMinima;
     }
    
-    /**
- * Restituisce il titolo del film.
+    /** Restituisce il titolo del film.
  * @return il titolo del film
  */
     public String getTitolo(){
         return titolo;
     }
 
-    /**
- * Restituisce il genere del film.
+    /** Restituisce il genere del film.
  * @return il genere del film
  */
     public String getGenere(){
         return genere;
     }
 
-    /**
- * Restituisce il regista del film.
+    /** Restituisce il regista del film.
  * @return il regista del film
  */
     public String getRegista(){
         return regista;
     }
 
-    /**
- * Restituisce l'anno del film.
+    /** Restituisce l'anno del film.
  * @return l'anno del film
  */
     public int getAnno(){
         return anno;
     }
 
-    /**
- * Restituisce la durata in minuti del film.
+    /** Restituisce la durata in minuti del film.
  * @return la durata in minuti del film
  */
     public int getDurataMinuti(){
         return durataMinuti;
     }
 
-    /**
- * Restituisce l'età minima del film.
+    /** Restituisce l'età minima del film.
  * @return l'età minima del film
  */
     public int getEtaMinima(){
         return etaMinima;
     }
 
-    /**
- * Restituisce una rappresentazione testuale dell'oggetto Film, utilizzata per la visualizzazione delle informazioni e per la memorizzazione dei dati nel file csv.
+    /** Restituisce una rappresentazione testuale dell'oggetto Film, utilizzata per la visualizzazione delle informazioni e per la memorizzazione dei dati nel file csv.
  * @return la rappresentazione testuale del film
  */
     @Override
@@ -95,6 +86,4 @@ public class Film {
         return " \" " +titolo + " \" " +"," +  genere +"," + " \" "+regista + " \" " +","+ anno +"," + durataMinuti + "," + etaMinima;
 
     }
-
-
 }
