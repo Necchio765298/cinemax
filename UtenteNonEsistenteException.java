9dff62571f3adee75fb52b37c6af86f911b29e9c
GaiaGalimberti <gaiagali2006@gmail.com>
2026-07-13 21:34:03 +0200

    aggiunto javadoc
    Added a custom exception for non-existent users with a detailed constructor.

--- UtenteNonEsistenteException.java
+++ UtenteNonEsistenteException.java
@@ -1,8 +1,13 @@
 package cinemax;
 
-
+/** Eccezione personalizzata lanciata quando un utente richiesto non è presente nel sistema.
+ * @author 
+ */
 public class UtenteNonEsistenteException extends Exception{
-	
+
+	/** Costruisce una nuova eccezione relativa a un utente non presente nel sistema.
+ * @param <id> identificativo dell'utente non trovato
+ */
 	public UtenteNonEsistenteException(long id){
 		System.out.println("L'utente con ID " + id+ " non esiste");
 	}
