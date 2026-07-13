c21b05bea6db1551700902c45bdcbbd9bc5db9e9
GaiaGalimberti <gaiagali2006@gmail.com>
2026-07-13 21:28:38 +0200

    aggiunto javadoc
    Added a custom exception for non-existent projections.

--- ProiezioneNonEsistenteException.java
+++ ProiezioneNonEsistenteException.java
@@ -1,7 +1,13 @@
 package cinemax;
 
+/** Eccezione personalizzata lanciata quando una proiezione richiesta non è presente nel sistema.
+ * @author 
+ */
 public class ProiezioneNonEsistenteException extends Exception{
 
+	/** Costruisce una nuova eccezione relativa a una proiezione non esistente.
+ * @param <Idt> data e ora della proiezione non trovata
+ */
 	public ProiezioneNonEsistenteException(LocalDateTime ldt){
 		System.out.println("La proiezione con data e ora " + ldt+ " non esiste");
 	}
