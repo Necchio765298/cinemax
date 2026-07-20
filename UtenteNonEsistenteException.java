a8adc0f7cd5f424de212fd368cbf944c75829091
GaiaGalimberti <gaiagali2006@gmail.com>
2026-07-20 13:49:44 +0200

    Fix comment formatting in UtenteNonEsistenteException

--- UtenteNonEsistenteException.java
+++ UtenteNonEsistenteException.java
@@ -1,12 +1,12 @@
 package cinemax;
 
-/** Eccezione personalizzata lanciata quando un utente richiesto non è presente nel sistema.
+/** Eccezione personalizzata lanciata quando un utente richiesto non è presente nel sistema
  * @author Arianna Necchio
  * @author Gaia Galimberti
  */
 public class UtenteNonEsistenteException extends Exception{
 
-	/** Costruisce una nuova eccezione relativa a un utente non presente nel sistema.
+	/** Costruisce una nuova eccezione relativa a un utente non presente nel sistema
  * @param <id> identificativo dell'utente non trovato
  */
 	public UtenteNonEsistenteException(long id){
