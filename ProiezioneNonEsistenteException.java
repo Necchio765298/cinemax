a403acec1722bea06a860a8853acb552e670f4d9
GaiaGalimberti <gaiagali2006@gmail.com>
2026-07-20 13:50:03 +0200

    Fix comments in ProiezioneNonEsistenteException

--- ProiezioneNonEsistenteException.java
+++ ProiezioneNonEsistenteException.java
@@ -1,16 +1,16 @@
 package cinemax;
 import java.time.LocalDateTime;
 
-/** Eccezione personalizzata lanciata quando una proiezione richiesta non è presente nel sistema.
+/** Eccezione personalizzata lanciata quando una proiezione richiesta non è presente nel sistema
  * @author Arianna Necchio
  * @author Gaia Galimberti
  */
 public class ProiezioneNonEsistenteException extends Exception{
 
-	/** Costruisce una nuova eccezione relativa a una proiezione non esistente.
+	/** Costruisce una nuova eccezione relativa a una proiezione non esistente
  * @param <Idt> data e ora della proiezione non trovata
  */
 	public ProiezioneNonEsistenteException(LocalDateTime ldt){
-		System.out.println("La proiezione con data e ora " + ldt+ " non esiste");
+		System.out.println("La proiezione con data e ora " + ldt + " non esiste");
 	}
 }
